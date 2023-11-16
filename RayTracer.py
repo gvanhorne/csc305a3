8f0af4ab6cac14edaf2b47212b72b9a5f2ceb873
Gregory Van Horne <Gregory.VanHorne@gov.bc.ca>
2023-11-16 12:48:40 -0800

    Initial commit

---
+++ RayTracer.py
@@ -0,0 +1,57 @@
+import sys
+def read_image_file(fp):
+  """
+  Reads an input file for an image and creates a dictionary for the pertinent values.
+
+  File Syntax:
+    NEAR <n>
+    LEFT <l>
+    RIGHT <r>
+    BOTTOM <b>
+    TOP <t>
+    RES <x> <y>
+    SPHERE <name> <pos x=""> <pos y=""> <pos z=""> <scl x=""> <scl y=""> <scl z="">
+            <r> <g> <b> <ka> <kd> <ks> <kr> <n>
+            // up to 14 additional sphere specifications
+    LIGHT <name> <pos x=""> <pos y=""> <pos z=""> <ir> <ig> <ib>
+            // up to 9 additional light specifications
+    BACK <r> <g> <b>
+    AMBIENT <ir> <ig> <ib>
+    OUTPUT <name>
+
+  Args:
+      fp (str): The path to the input file.
+
+  Returns:
+      dict: A dictionary containing the entries from the input file.
+  """
+  try:
+    scene_dict = {}
+  
+    with open(fp, 'r') as f:
+      for line in f:
+        line = line.strip().split()
+        
+        # Skip empty lines
+        if not line:
+            continue
+        
+        key = line[0]
+        if (len(line) == 2):
+          value = line[1]
+        else:
+          value = line[1:]
+        scene_dict[key] = value
+        
+  except FileNotFoundError:
+      print(f"Error: File '{fp}' not found.")
+    
+  return scene_dict
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 RayTracer.py <testcase>.txt")
+        sys.exit(1)
+    fp = sys.argv[1]
+    scene_dict = read_image_file(fp)
+    print(scene_dict)
