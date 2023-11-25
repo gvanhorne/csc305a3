9de8f22ecd055982835ed2229a0ef469baeff092
Gregory Van Horne <Gregory.VanHorne@gov.bc.ca>
2023-11-25 15:38:15 -0800

    Add constructor method for sphere

--- sphere.py
+++ sphere.py
@@ -3,6 +3,7 @@ class Sphere:
     Represents a sphere in a 3D space.
 
     Attributes:
+    - name (str): String for the name to be used by the sphere
     - position (tuple): Float tuple representing the position (x, y, z) of the sphere.
     - scaling (tuple): Float tuple representing the non-uniform scaling factors (sx, sy, sz) of the sphere.
     - color (tuple): Three-star float tuple representing the color (r, g, b) of the sphere.
@@ -17,7 +18,7 @@ class Sphere:
     ...                 ka=0.1, kd=0.7, ks=0.2, kr=0.1, n=10)
     """
 
-    def __init__(self, position, scaling, color, ka, kd, ks, kr, n):
+    def __init__(self, name, position, scaling, color, ka, kd, ks, kr, n):
         """
         Initializes a Sphere object with the provided attributes.
 
@@ -31,6 +32,7 @@ class Sphere:
         - kr (float): Coefficient for reflection.
         - n (int): Specular exponent.
         """
+        self.name = name
         self.position = position
         self.scaling = scaling
         self.color = color
@@ -39,3 +41,27 @@ class Sphere:
         self.ks = ks
         self.kr = kr
         self.n = n
+
+    @classmethod
+    def from_array(cls, input_string):
+        """
+        Create a Sphere object from the provided input string.
+
+        Parameters:
+        - input_string (list): Input string in the format ['name', 'pos x', 'pos y', 'pos z', 'scl x', 'scl y', 'scl z', 'r', 'g', 'b', 'ka', 'kd', 'ks', 'kr', 'n'].
+
+        Returns:
+        Sphere: Initialized Sphere object.
+        """
+        name = input_string[0]
+        position = tuple(map(float, input_string[1:4]))
+        scaling = tuple(map(float, input_string[4:7]))
+        color = tuple(map(float, input_string[7:10]))
+        ka, kd, ks, kr, n = map(float, input_string[10:])
+        return cls(name, position, scaling, color, ka, kd, ks, kr, n)
+
+    def __str__(self):
+        """
+        Returns a string representation of the Sphere object.
+        """
+        return f"Sphere {self.name}: position={self.position}, scaling={self.scaling}, color={self.color}, ka={self.ka}, kd={self.kd}, ks={self.ks}, kr={self.kr}, n={self.n}"
