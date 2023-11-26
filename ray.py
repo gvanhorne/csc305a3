f2295f67eff2e5604364321756390a1d47e22675
Gregory Van Horne <Gregory.VanHorne@gov.bc.ca>
2023-11-25 22:04:06 -0800

    Add normalization method for rays

--- ray.py
+++ ray.py
@@ -1,11 +1,38 @@
-class Ray:
-  def __init__(self, origin, direction):
-    self.origin = origin
-    self.direction = direction
+from typing import Tuple
+import math
 
-  def at(self, t):
+class Ray:
+  def __init__(self, origin: Tuple[float, float, float], direction: Tuple[float, float, float]):
+    """
+    Initialize a Ray object with an origin point and a direction vector.
+
+    Parameters:
+    - origin (tuple): A tuple representing the (x, y, z) coordinates of the origin point.
+    - direction (tuple): A tuple representing the (I, j, k) components of the direction vector.
+    """
+    self.origin = tuple(origin)
+    self.direction = tuple(direction)
+
+  def at(self, t: float) -> Tuple[float, float, float]:
     """
     Calculate a point along the ray at a given parameter t.
+
+    Parameters:
+    - t (float): The parameter value indicating the position along the ray.
+
+    Returns:
+    tuple: A tuple representing the (x, y, z) coordinates of the point along the ray at parameter t.
     """
     scaled_direction = tuple(t * d for d in self.direction)
     return tuple(o + sd for o, sd in zip(self.origin, scaled_direction))
+
+  def normalized_direction(self) -> Tuple[float, float, float]:
+    """
+    Return the normalized direction vector.
+
+    Returns:
+    Tuple[float, float, float]: The normalized direction vector.
+    """
+    length = math.sqrt(sum(component**2 for component in self.direction))
+    normalized_vector = tuple(component / length for component in self.direction)
+    return normalized_vector
