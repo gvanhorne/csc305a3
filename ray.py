b48a3432a114060774563fd8a1645b3757b6c48a
Gregory Van Horne <Gregory.VanHorne@gov.bc.ca>
2023-11-26 14:06:05 -0800

    Add function to colour pixel by ray

--- ray.py
+++ ray.py
@@ -1,4 +1,5 @@
-from vector import Vector
+from vector import Vector, unit_vector
+from colour import Colour
 
 class Ray:
   def __init__(self, origin: Vector, direction: Vector):
@@ -24,3 +25,14 @@ class Ray:
     """
     scaled_direction = Vector(t * self.direction.x(), t * self.direction.y(), t * self.direction.z())
     return Vector(self.origin.x() + scaled_direction.x(), self.origin.y() + scaled_direction.y(), self.origin.z() + scaled_direction.z())
+
+  def get_colour(self) -> Colour:
+    """
+    Get the color based on the y direction of the ray.
+
+    Returns:
+    Colour: The computed color based on the direction of the ray.
+    """
+    unit_direction = unit_vector(self.direction)
+    a = 0.5*(unit_direction.y() + 1.0)
+    return (1.0-a)*Colour(1.0, 1.0, 1.0) + a*Colour(0.5, 0.7, 1.0)
