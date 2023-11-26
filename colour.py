e08bd0a209a3328a24b6bf0d9fbc39502ca9c24e
Gregory Van Horne <Gregory.VanHorne@gov.bc.ca>
2023-11-26 14:04:47 -0800

    Add addition and multiplication for colours

--- colour.py
+++ colour.py
@@ -12,6 +12,42 @@ class Colour:
     self.g = g
     self.b = b
 
+  def __add__(self, other: 'Colour') -> 'Colour':
+    """
+    Override the addition operation for Colour instances.
+
+    Parameters:
+    - other (Colour): The Colour to add.
+
+    Returns:
+    Colour: A new Colour representing the result of the addition.
+    """
+    return Colour(self.r + other.r, self.g + other.g, self.b + other.b)
+
+  def __mul__(self, scalar: float) -> 'Colour':
+    """
+    Multiply the Colour by a scalar.
+
+    Parameters:
+    - scalar (float): The scalar factor.
+
+    Returns:
+    Colour: A new Colour representing the result of scalar multiplication.
+    """
+    return Colour(self.r * scalar, self.g * scalar, self.b * scalar)
+
+  def __rmul__(self, scalar: float) -> 'Colour':
+    """
+    Multiply the Colour by a scalar (commutative).
+
+    Parameters:
+    - scalar (float): The scalar factor.
+
+    Returns:
+    Colour: A new Colour representing the result of scalar multiplication.
+    """
+    return self.__mul__(scalar)
+
   def __repr__(self) -> str:
     """
     Return a string representation of the Colour object.
